12fa56d7aa1647a760a0e959833537b8f85f30e0
Shirley <shirleey@umich.edu>
2018-09-22 00:54:10 -0400

    add class_card

--- go_fish.py
+++ go_fish.py
@@ -1,2 +1,18 @@
+import random
 
+class Card(object):
+	suit_names =  ["Diamonds","Clubs","Hearts","Spades"]
+	rank_levels = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+	faces = {1:"Ace",11:"Jack",12:"Queen",13:"King"}
+
+	def __init__(self, suit=0,rank=2):
+		self.suit = self.suit_names[suit]
+		if rank in self.faces: # self.rank handles printed representation
+			self.rank = self.faces[rank]
+		else:
+			self.rank = rank
+		self.rank_num = rank # To handle winning comparison
+
+	def __str__(self):
+		return "{} of {}".format(self.rank,self.suit)
 
