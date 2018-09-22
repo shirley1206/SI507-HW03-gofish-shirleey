cf02c56b538e68470628f5cff4c0b16579a103bc
tifwu <tifwu@umich.edu>
2018-09-22 00:51:00 -0400

    add deck class

--- go_fish.py
+++ go_fish.py
@@ -1,2 +1,68 @@
+import random
 
+class Deck(object):
+    def __init__(self):  # Don't need any input to create a deck of cards
+        # This working depends on Card class existing above
+        self.cards = []
+        for suit in range(4):
+            for rank in range(1, 14):
+                card = Card(suit, rank)
+                self.cards.append(card)  # appends in a sorted order
+
+    def __str__(self):
+        total = []
+        for card in self.cards:
+            total.append(card.__str__())
+        # shows up in whatever order the cards are in
+        return "\n".join(total)  # returns a multi-line string listing each card
+
+    def pop_card(self, i=-1):
+        # removes and returns a card from the Deck
+        # default is the last card in the Deck
+        return self.cards.pop(i)  # this card is no longer in the deck -- taken off
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+    def replace_card(self, card):
+        card_strs = []  # forming an empty list
+        for c in self.cards:  # each card in self.cards (the initial list)
+            card_strs.append(c.__str__())  # appends the string that represents that card to the empty list
+        if card.__str__() not in card_strs:  # if the string representing this card is not in the list already
+            self.cards.append(card)  # append it to the list
+
+    def sort_cards(self):
+        # Basically, remake the deck in a sorted way
+        # This is assuming you cannot have more than the normal 52 cars in a deck
+        self.cards = []
+        for suit in range(4):
+            for rank in range(1, 14):
+                card = Card(suit, rank)
+                self.cards.append(card)
+
+
+    def deal(self, total_hands):
+        player_hand = []
+        return_hands = []
+        deck = Deck()
+        cards_per_hand = 7
+
+        if cards_per_hand != -1:
+            for i in range(total_hands):
+                for j in range(cards_per_hand):
+                    card = deck.pop_card()
+                    player_hand.append(card)
+                return_hands.append(Hand(player_hand))
+                player_hand = [] # reinitialize cards on hand for next player
+        else:
+            all_card_per_hand = round(len(deck.cards)/total_hands)
+            for i in range(total_hands):
+                for j in range(all_card_per_hand):
+                    card = deck.pop_card()
+                    player_hand.append(card)
+                return_hands.append(Hand(player_hand))
+                # print(len(Hand(player_hand).cards))
+                player_hand = [] # reinitialize cards on hand for next player
+
+        return return_hands
 
